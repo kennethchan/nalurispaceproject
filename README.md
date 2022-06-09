998431d201873069802e769392eb4050c8a72478
Kenneth <kennethchan.dev@gmail.com>
2022-06-09 22:47:24 +0800

    added area, surfacearea, volume + cleanup + readme

--- README.md
+++ README.md
@@ -1,1 +1,47 @@
-# nalurispaceproject
+# Naluri Space Project
+
+Welcome to the Naluri Space Project's Planet Solar System Modal application. This is a preliminary approach to providing useful data and visual aid for you to view the planets in our solar system.
+
+## Features List
+
+ - Pi value calculation in real-time intervals
+ - Solar system planets selection
+ - Visual references to planets
+ - Planet data (Circumference, Area, Surface Area, Volume)
+ - Pi value fetching
+
+## Installation
+
+1. Clone the project / Unzip to directory
+2. Open Command Prompt/Terminal and navigate to folder 'nsp-node-server'
+3. Run 'npm install'
+4. Navigate to folder 'nsp-client'
+5. Run 'npm install'
+
+## Usage
+
+1. Open two instances of Command Prompt/Terminal
+2. First instance navigate to folder 'nsp-node-server'
+3. Second instance navigate to folder 'nsp-client'
+4. In each instance, run 'npm start'
+5. The node server will run and listen onto port 3001 (Feel free to change in case of clashes)
+6. The client will launch in a browser at 'http://localhost:3000/'
+
+## Selecting Planet
+
+1. Select a planet using the dropdown menu 
+or
+2. Swipe to a target planet using the slider 
+or
+4. Use the arrow keys to select a target planet
+5. Relevant and useful data will be calculated and shown once a planet is selected
+
+## Updating Pi Value
+
+1. Pi value will be updated upon a planet's selection
+2. Pi value may also be manually updated by clicking the 'Update Pi Value' button
+
+## Changing Port Number
+
+1. Navigate to 'nsp-node-server/server/index.js
+2. edit the port number in 'const  PORT = process.env.PORT || 3001' to whichever is available in your network
--- nsp-client/src/App.js
+++ nsp-client/src/App.js
@@ -1,97 +1,157 @@
-import React, { image, useEffect } from "react";
-import "./App.css";
+import React from "react";
 import axios from 'axios';
 import radiusReference from './radiusReference'
 import Dropdown from 'react-bootstrap/Dropdown'
-import { Container, Row, Col } from 'react-bootstrap'
+import { Container, Row, Col, Button } from 'react-bootstrap'
 import Slider from "react-slick";
 
-function App() {
-  const [piVal, setPiVal] = React.useState(0);
-  const [planet, setPlanet] = React.useState('Select Planet');
-  const [circumference, setCircumference] = React.useState(0);
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      piVal: 0,
+      planet: 'Select Planet',
+      circumference: 0,
+      count: 0,
+      area: 0,
+      surfaceArea: 0,
+      volume: 0
+    };
+  }
 
+  componentDidMount = () => {
+    this.getCurrentPiVal()
+  }
 
-  React.useEffect(() => {
-    getCurrentPiVal()
-  }, []);
-
-  const getCurrentPiVal = () => {
+  //Utilizing Axios to use a get API request for the latest Pi value
+  getCurrentPiVal = () => {
     axios.get('/getCurrentPiVal')
       .then(res => {
-        setPiVal(res.data.message)
+        this.setState({ piVal: res.data.message })
       }).catch(err => { console.log(err) })
   }
 
-  const handlePlanetSelect = (planet) => {
-    getCurrentPiVal()
-    setPlanet(planet)
-    calcCircumference(planet)
+  //Handler function to update the Pi value, calculate the selected planet's circumference, 
+  //update the dropdown value and move the slider to the correct image
+  handlePlanetSelect = (planet) => {
+    this.getCurrentPiVal()
+    if (radiusReference.some(e => e.planetName === planet)) {
+      this.calcCircumference(planet)
+      this.calcArea(planet)
+      this.calcSurfaceArea(planet)
+      this.calcVolume(planet)
+      this.slider.slickGoTo(radiusReference.findIndex(e => e.planetName === planet))
+      this.setState({ planet: planet })
+    }
   }
 
-  const calcCircumference = (planet) => {
-    let value = piVal * radiusReference.find(e => e.planetName == planet).radius * 2
-    setCircumference(value.toFixed(2))
+  //Calculates the selected planet's circumference
+  calcCircumference = (planet) => {
+    let value = this.state.piVal * radiusReference.find(e => e.planetName === planet).radius * 2
+    this.setState({ circumference: value.toFixed(2) })
   }
 
-  var settings = {
-    infinite: true,
-    slidesToShow: 1,
-    slidesToScroll: 1
-  };
+  //Calculates the selected planet's area
+  calcArea = (planet) => {
+    if (radiusReference.some(e => e.planetName === planet)) {
+      let value = this.state.piVal * Math.pow(radiusReference.find(e => e.planetName === planet).radius, 2)
+      this.setState({ area: value.toFixed(2) })
+    }
+  }
 
-  return (
-    <Container className="cont-header py-5" fluid>
-      <Row className="justify-content-center">
-        <p className="title">Naluri Space Project</p>
-      </Row>
-      <Row className="justify-content-center">
-        <Col xs={12} md={8} className='py-5'>
-          <Slider {...settings} afterChange={(e) => { handlePlanetSelect(radiusReference[e].planetName) }}>
-            <img src={require('./assets/Sun.gif')} className='slider-image' />
-            <img src={require('./assets/Mercury.gif')} className='slider-image' />
-            <img src={require('./assets/Venus.gif')} className='slider-image' />
-            <img src={require('./assets/Earth.gif')} className='slider-image' />
-            <img src={require('./assets/Mars.gif')} className='slider-image' />
-            <img src={require('./assets/Jupiter.gif')} className='slider-image' />
-            <img src={require('./assets/Saturn.gif')} className='slider-image' />
-            <img src={require('./assets/Uranus.gif')} className='slider-image' />
-            <img src={require('./assets/Neptune.gif')} className='slider-image' />
-            <img src={require('./assets/Pluto.gif')} className='slider-image' />
-          </Slider>
-        </Col>
-        <Col xs={12} md={8}>
-          <p className="circumference">Circumference of</p>
-        </Col>
-        <Col xs={12} md={8}>
-          <Dropdown>
-            <Dropdown.Toggle variant="primary" className="planet-selector">
-              {planet}
-            </Dropdown.Toggle>
-            <Dropdown.Menu>
-              <Dropdown.Item onClick={() => { handlePlanetSelect('Sun') }}>Sun</Dropdown.Item>
-              <Dropdown.Item onClick={() => { handlePlanetSelect('Mercury') }}>Mercury</Dropdown.Item>
-              <Dropdown.Item onClick={() => { handlePlanetSelect('Venus') }}>Venus</Dropdown.Item>
-              <Dropdown.Item onClick={() => { handlePlanetSelect('Earth') }}>Earth</Dropdown.Item>
-              <Dropdown.Item onClick={() => { handlePlanetSelect('Mars') }}>Mars</Dropdown.Item>
-              <Dropdown.Item onClick={() => { handlePlanetSelect('Jupiter') }}>Jupiter</Dropdown.Item>
-              <Dropdown.Item onClick={() => { handlePlanetSelect('Saturn') }}>Saturn</Dropdown.Item>
-              <Dropdown.Item onClick={() => { handlePlanetSelect('Uranus') }}>Uranus</Dropdown.Item>
-              <Dropdown.Item onClick={() => { handlePlanetSelect('Neptune') }}>Neptune</Dropdown.Item>
-              <Dropdown.Item onClick={() => { handlePlanetSelect('Pluto') }}>Pluto {`(debatable)`}</Dropdown.Item>
-            </Dropdown.Menu>
-          </Dropdown>
-        </Col>
-        <Col xs={12} md={8} className='pt-3'>
-          <p className="circumference">{Number(circumference).toFixed(2)
-            .toString()
-            .replace(/\B(?=(\d{3})+(?!\d))/g, ',')} km
-          </p>
-        </Col>
+  //Calculates the selected planet's surface area
+  calcSurfaceArea = (planet) => {
+    if (radiusReference.some(e => e.planetName === planet)) {
+      let value = this.state.piVal * Math.pow(radiusReference.find(e => e.planetName === planet).radius, 2) * 4
+      this.setState({ surfaceArea: value.toFixed(2) })
+    }
+  }
 
-      </Row>
-    </Container>
-  );
+  //Calculates the selected planet's volume
+  calcVolume = (planet) => {
+    if (radiusReference.some(e => e.planetName === planet)) {
+      let value = this.state.piVal * Math.pow(radiusReference.find(e => e.planetName === planet).radius, 3) * (4 / 3)
+      this.setState({ volume: value.toFixed(2) })
+    }
+  }
+
+  //Renders dropdown item's programmatically for cleaner code
+  renderDropdownItems = () => {
+    return radiusReference.map((e, i) => {
+      return (
+        <Dropdown.Item onClick={() => { this.handlePlanetSelect(e.planetName) }} key={i}>{e.planetName}</Dropdown.Item>
+      )
+    })
+  }
+
+  //Renders images programmatically for cleaner code
+  renderImages = () => {
+    return radiusReference.map((e, i) => {
+      return (
+        <img src={require(`./assets/${e.planetName}.gif`)} className='slider-image' alt="" key={i} />
+      )
+    })
+  }
+
+
+  render() {
+    return (
+      <Container className="cont-header py-5" fluid>
+        <Row className="justify-content-center">
+          <p className="title">Naluri Space Project</p>
+        </Row>
+        <Row className="justify-content-center">
+          <Col xs={12} md={8} className='py-5'>
+            <Slider ref={c => (this.slider = c)} settings={{
+              infinite: true,
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }} afterChange={(e) => { this.handlePlanetSelect(radiusReference[e].planetName) }}>
+              {this.renderImages()}
+            </Slider>
+          </Col>
+          <Col xs={12} md={8}>
+            <Dropdown>
+              <Dropdown.Toggle variant="primary" className="planet-selector">
+                {this.state.planet}
+              </Dropdown.Toggle>
+              <Dropdown.Menu>
+                {this.renderDropdownItems()}
+              </Dropdown.Menu>
+            </Dropdown>
+          </Col>
+          {this.state.circumference > 0 && <Col xs={12} md={8} className='pt-3'>
+            <p className="data">Circumference: {Number(this.state.circumference).toFixed(2)
+              .toString()
+              .replace(/\B(?=(\d{3})+(?!\d))/g, ',')} km
+            </p>
+          </Col>}
+          {this.state.area > 0 && <Col xs={12} md={8}>
+            <p className="data">Area: {Number(this.state.area).toFixed(2)
+              .toString()
+              .replace(/\B(?=(\d{3})+(?!\d))/g, ',')} km²
+            </p>
+          </Col>}
+          {this.state.surfaceArea > 0 && <Col xs={12} md={8}>
+            <p className="data">Surface Area: {Number(this.state.surfaceArea).toFixed(2)
+              .toString()
+              .replace(/\B(?=(\d{3})+(?!\d))/g, ',')} km²
+            </p>
+          </Col>}
+          {this.state.volume > 0 && <Col xs={12} md={8}>
+            <p className="data">Volume: {Number(this.state.volume).toFixed(2)
+              .toString()
+              .replace(/\B(?=(\d{3})+(?!\d))/g, ',')} km³
+            </p>
+          </Col>}
+          <Col xs={12} md={8} className='pt-3 d-flex flex-column justify-content-center align-items-center'>
+            <textarea defaultValue={this.state.piVal} className='textArea' />
+            <Button className='updateBtn mt-3' onClick={() => {
+              this.handlePlanetSelect(this.state.planet)
+            }}>Update Pi Value</Button>
+          </Col>
+        </Row>
+      </Container >
+    );
+  }
 }
-
-export default App;
